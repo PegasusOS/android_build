4e133d9a85e0d426300188d955051ae25d5362d1
althafvly <althafvly@gmail.com>
2021-04-13 07:14:41 +0000

    build: add pegasus support
     Remove unwanted apps

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -36,4 +36,4 @@ PRODUCT_PACKAGES += \
     PhotoTable \
     WallpaperPicker \
 
-PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+#PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
